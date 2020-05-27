c89e603732c9d76c7bb81bc556afd13ebd9c05ed
金绍港 <shaogang.jsg@cainiao.com>
2020-05-27 11:55:40 +0800

    add atss r50

--- configs/wheat/atss_r50.py
+++ configs/wheat/atss_r50.py
@@ -1,21 +1,20 @@
+# model settings
 model = dict(
     type='ATSS',
-    pretrained='https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net101_v1b_26w_4s-0812c246.pth',
+    pretrained='torchvision://resnet50',
     backbone=dict(
-        type='Res2Net',
-        depth=101,
-        scale=4,
-        baseWidth=26,
+        type='ResNet',
+        depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        #norm_cfg=dict(type='BN', requires_grad=False),
+        norm_cfg=dict(type='BN', requires_grad=True),
         style='pytorch'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=0,
+        start_level=1,
         add_extra_convs=True,
         extra_convs_on_inputs=False,
         num_outs=5),
@@ -28,7 +27,7 @@ model = dict(
         octave_base_scale=8,
         scales_per_octave=1,
         anchor_ratios=[1.0],
-        anchor_strides=[4, 8, 16, 32, 64],
+        anchor_strides=[8, 16, 32, 64, 128],
         target_means=[.0, .0, .0, .0],
         target_stds=[0.1, 0.1, 0.2, 0.2],
         loss_cls=dict(
@@ -50,93 +49,23 @@ test_cfg = dict(
     nms_pre=1000,
     min_bbox_size=0,
     score_thr=0.05,
-    nms=dict(type='nms', iou_thr=0.7),
-    max_per_img=200)
+    nms=dict(type='nms', iou_thr=0.6),
+    max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/wheat/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
-albu_train_transforms = [
-    dict(
-        type='RandomBrightnessContrast',
-        brightness_limit=[0.1, 0.3],
-        contrast_limit=[0.1, 0.3],
-        p=0.2),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='RGBShift',
-                r_shift_limit=10,
-                g_shift_limit=10,
-                b_shift_limit=10,
-                p=1.0),
-            dict(
-                type='HueSaturationValue',
-                hue_shift_limit=20,
-                sat_shift_limit=30,
-                val_shift_limit=20,
-                p=1.0)
-        ],
-        p=0.1),
-    dict(type='JpegCompression', quality_lower=85, quality_upper=95, p=0.2),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(type='Blur', blur_limit=3, p=1.0),
-            dict(type='MedianBlur', blur_limit=3, p=1.0)
-        ],
-        p=0.1),
-]
-
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
-    dict(type='Pad', size_divisor=32),
-    dict(
-        type='Albu',
-        transforms=albu_train_transforms,
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_labels'],
-            min_visibility=0.0,
-            filter_lost_elements=True),
-        keymap={
-            'img': 'image',
-            'gt_bboxes': 'bboxes'
-        },
-        update_pad_shape=False,
-        skip_img_without_anno=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='DefaultFormatBundle'),
-    dict(
-        type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels'],
-        meta_keys=('filename', 'ori_shape', 'img_shape', 'img_norm_cfg',
-                   'pad_shape', 'scale_factor'))
-]
-'''
-
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(1333, 800), (1333, 1024)], keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
-'''
-
-
-
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -194,7 +123,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/atss'
+work_dir = './work_dirs/atss_r50_fpn_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
--- train_atss.sh
+++ train_atss.sh
@@ -1,1 +1,1 @@
-sh ./tools/dist_train.sh  configs/wheat/atss.py 4 --launcher pytorch 
+sh ./tools/dist_train.sh  configs/wheat/atss_r50.py 4 --launcher pytorch 
